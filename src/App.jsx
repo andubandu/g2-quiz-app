d637e2906e84ae81d17cd8e8fb8d69cf60370a70
andubandu <lolandu575@gmail.com>
2025-03-12 17:14:25 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ function App() {
     >
       <div className="container mx-auto px-4 py-8">
         <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        
       </div>
     </div>
   );
